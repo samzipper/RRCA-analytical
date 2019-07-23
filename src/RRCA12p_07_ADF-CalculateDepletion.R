434fab90a044c4a623eca98d8f8b606d4195be33
Sam Zipper <samuelczipper@gmail.com>
2019-07-23 07:27:04 -0700

    Correct filename in ADF-CalculateDepletion

--- src/RRCA12p_07_ADF-CalculateDepletion.R
+++ src/RRCA12p_07_ADF-CalculateDepletion.R
@@ -250,7 +250,7 @@ for (w in 1:length(wells_all)){
 }
 
 ## save output
-fname <- paste0("RRCA12p_07_ADF-CalculationDepletion_", analytical_model, "_", apportionment, "_", paste(str_BCs, collapse = "-"), ".csv")
+fname <- paste0("RRCA12p_07_ADF-CalculationDepletion_", analytical_model, "_", storage, "_", apportionment, "_", paste(str_BCs, collapse = "-"), ".csv")
 depletion_all %>% 
   dplyr::select(WellNum, SegNum, time_days, Qw_m3d, frac_depletion, Qa, depletion_m3d) %>% 
   dfDigits() %>% 
